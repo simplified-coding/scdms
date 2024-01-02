56494cb045803f011cbf91a23d0e9e06108c40f1
GalaxyGamingBoy <xrteach@gmail.com>
2024-01-02 13:49:58 +0000

    feat: Add swagger UI

--- scdms-backend/src/index.ts
+++ scdms-backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import swaggerUI from "swagger-ui-express"
 
 import rCertificates from "./api/certificates"
+
+import swaggerDocument from "./assets/OpenAPI.json"
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // Routes
 app.use('/certs', rCertificates)
